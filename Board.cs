2ad91f5eff00139efe34ffcea5a0b7746a7d2750
AndyPietr <siakax@gmail.com>
2023-07-18 19:00:29 +0200

    added board class

---
+++ Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe {
+    internal class Board {
+        private char[,] fields;
+
+        public Board()
+        {
+            this.fields = new char[,]{
+                {'1', '2', '3'},
+                {'4', '5', '6' },
+                {'7', '8', '9' }
+            };
+        }
+
+
+    }
+}
